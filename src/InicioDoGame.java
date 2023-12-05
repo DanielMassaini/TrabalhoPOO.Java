2678ac9b34c2e99041909031cf252b508b82a64f
DanielMassaini <110943950+DanielMassaini@users.noreply.github.com>
2023-12-05 12:13:28 +0100

    Update InicioDoGame.java

--- src/InicioDoGame.java
+++ src/InicioDoGame.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+
 public class inicioDoJogo {
 
+    public static void main(String[] args) {
+        Jogo jogo = new Jogo(); // Supondo que exista uma classe Jogo com o método inicioDoJogo
+
+        // Chamar o método inicioDoJogo da classe Jogo
+        jogo.inicioDoJogo();
+    }
 }
