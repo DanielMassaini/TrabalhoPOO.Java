803651d58fbfbdaba86f5109566a75b2ff395ad6
DanielMassaini <110943950+DanielMassaini@users.noreply.github.com>
2023-12-05 12:10:47 +0100

    Update SorteioDaLetra.java

--- src/SorteioDaLetra.java
+++ src/SorteioDaLetra.java
@@ -2,4 +2,10 @@ import java.util.Random;
 
 public class SorteioDaLetra {
 
+    private static final String ALFABETO = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public char sortearLetra() {
+        Random random = new Random();
+        return ALFABETO.charAt(random.nextInt(ALFABETO.length()));
+    }
 }
